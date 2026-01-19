919d0e9743c8a1e062d3f70040823632af688768
Ericlee <paranormal91@live.com>
2026-01-19 00:30:50 -0600

    delete wrong nested auth page

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: "system-ui" }}>
-      <h1>AITube is live ✅</h1>
-      <p>If you see this, the / route is working.</p>
-    </div>
-  );
-}
